c97a21cfce9656302c93f543583b448c0194c60d
w <kienhue98@gmail.com>
2023-08-10 23:13:54 +0700

    man hinh hien thi dai ly

---
+++ app/Http/Controllers/backend/AgencyController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\backend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Agency;
+use Illuminate\Http\Request;
+
+class AgencyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $listDaiLy = Agency::where('status', 1)->get(['id', 'ten_co_so', 'ten_quan_ly']);
+        return view('backend/pages/dai_ly/index', compact('listDaiLy'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('backend/pages/dai_ly/create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        dd($request->input());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
