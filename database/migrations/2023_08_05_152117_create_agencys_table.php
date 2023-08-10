8bb22d28bb55e92df555ff325993b5d73029afc5
w <kienhue98@gmail.com>
2023-08-10 23:12:27 +0700

    sua migrate agencies

--- database/migrations/2023_08_05_152117_create_agencys_table.php
+++ database/migrations/2023_08_05_152117_create_agencys_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('agencys', function (Blueprint $table) {
+        Schema::create('agencies', function (Blueprint $table) {
             $table->id();
             $table->string('user_id');
             $table->string('ten_quan_ly')->nullable();
