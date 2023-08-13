d664a6a07e82b8a69fc708c8764b5c698bb83162
w <kienhue98@gmail.com>
2023-08-13 22:24:06 +0700

    them migrate

---
+++ database/migrations/2023_08_13_064350_insert_column_to_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->boolean('ban_chay')->nullable();
+            $table->boolean('yeu_thich')->nullable();
+            $table->boolean('hot')->nullable();
+            $table->boolean('flash_sale')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+};
