f5c60d57fc090e72edfba2aacff01f74f46a6bf1
w <kienhue98@gmail.com>
2023-08-13 22:25:06 +0700

    chuc nang dang nhap

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Enum\UserStatus;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+
+class AuthController extends Controller
+{
+
+    public function showFormLogin()
+    {
+        return view('backend.pages.dang-nhap.index');
+    }
+
+    public function login(Request $request)
+    {
+        $emailLogin = $request->input('email');
+
+            $credentials = [
+                'email' => $emailLogin,
+                'password' => $request->input('password'),
+            ];
+
+        $user = User::where('email' , $emailLogin)->first();
+
+
+        if ($user && $user->status !== UserStatus::ACTIVE) {
+            return back()->with('error', 'Tài khoản của bạn đã bị khóa.');
+        }
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->put('login', $emailLogin);
+            $login = $request->session()->get('login');
+            return redirect()->route('backend.thong-ke.show');
+        } else {
+            return back()->with('error', 'Tên đăng nhập hoặc mật khẩu không chính xác');
+        }
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        Session::forget('login');
+
+        return route('login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\backend\AgencyController;
 use App\Http\Controllers\backend\BackendController;
 use App\Http\Controllers\backend\DonHangController;
@@ -23,33 +24,43 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/login', [AuthController::class, 'showFormLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('sign-in');
+Route::post('/logout', [AuthController::class, 'logout'])->name('log-out');
 
 
+Route::middleware(['auth'])->group(function () {
 
-Route::prefix('/backend')->group(function () {
-    Route::get('/dang-nhap', [BackendController::class, 'login'])->name('backend.dang-nhap.show');
-    Route::get('/dai-ly', [AgencyController::class, 'index'])->name('backend.dai-ly.show');
-    Route::get('/thong-ke', [ThongKeController::class, 'index'])->name('backend.thong-ke.show');
-    Route::get('/tin-nhan', [TinNhanController::class, 'index'])->name('backend.tin-nhan.show');
-    Route::get('/email', [QLEmailController::class, 'index'])->name('backend.email.show');
-    Route::get('/nguoi-dung', [QL_UserController::class, 'index'])->name('backend.user.show');
-    Route::get('/ma-giam-gia', [QL_VoucherController::class, 'index'])->name('backend.voucher.show');
-    Route::get('/san-pham', [ProductController::class, 'index'])->name('backend.san-pham.show');
-    Route::get('/dich-vu-them', [MoreServiceController::class, 'index'])->name('backend.dich-vu-them.show');
-    Route::get('/trang-ca-nhan', [ProfileController::class, 'index'])->name('backend.trang-ca-nhan.show');
-    Route::get('/don-hang', [DonHangController::class, 'index'])->name('backend.don-hang.show');
+
+    Route::prefix('/backend')->group(function () {
+        Route::get('/dai-ly', [AgencyController::class, 'index'])->name('backend.dai-ly.show');
+        Route::get('/thong-ke', [ThongKeController::class, 'index'])->name('backend.thong-ke.show');
+        Route::get('/tin-nhan', [TinNhanController::class, 'index'])->name('backend.tin-nhan.show');
+        Route::get('/email', [QLEmailController::class, 'index'])->name('backend.email.show');
+        Route::get('/nguoi-dung', [QL_UserController::class, 'index'])->name('backend.user.show');
+        Route::get('/ma-giam-gia', [QL_VoucherController::class, 'index'])->name('backend.voucher.show');
+        Route::get('/san-pham', [ProductController::class, 'index'])->name('backend.san-pham.show');
+        Route::get('/dich-vu-them', [MoreServiceController::class, 'index'])->name('backend.dich-vu-them.show');
+        Route::get('/trang-ca-nhan', [ProfileController::class, 'index'])->name('backend.trang-ca-nhan.show');
+        Route::get('/don-hang', [DonHangController::class, 'index'])->name('backend.don-hang.show');
+    });
+    Route::prefix('/backend/nguoi-dung')->group(function () {
+        Route::post('/store', [QL_UserController::class, 'store'])->name('backend.nguoi-dung.store');
+        Route::get('/show/{id}', [QL_UserController::class, 'show'])->name('backend.nguoi-dung.show');
+        Route::post('/update/{id}', [QL_UserController::class, 'update'])->name('backend.nguoi-dung.update');
+
+    });
+
+    Route::prefix('/backend/san-pham')->group(function () {
+        Route::get('/create', [ProductController::class, 'create'])->name('backend.san-pham.create');
+        Route::post('/store', [ProductController::class, 'store'])->name('backend.san-pham.store');
+    });
+
+    Route::prefix('/backend/dich-vu-them')->group(function () {
+        Route::post('/store', [MoreServiceController::class, 'store'])->name('backend.dich-vu-them.store');
+        Route::get('/get-by-agency/{id}', [MoreServiceController::class, 'getAllByAgency'])->name('backend.dich-vu-them.get-by-agency');
+
+    });
 });
-Route::prefix('/backend/nguoi-dung')->group(function () {
-    Route::post('/store', [QL_UserController::class, 'store'])->name('backend.nguoi-dung.store');
-});
-
-Route::prefix('/backend/san-pham')->group(function () {
-    Route::get('/create', [ProductController::class, 'create'])->name('backend.san-pham.create');
-});
-
-Route::prefix('/backend/dich-vu-them')->group(function () {
-    Route::post('/store', [MoreServiceController::class, 'store'])->name('backend.dich-vu-them.store');
-});
-
 
 
