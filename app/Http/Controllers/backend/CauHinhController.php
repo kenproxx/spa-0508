1ac4582e7af1dd261f26945ea7a47b756d62c460
kiennt <kienhue98@gmail.com>
2023-08-16 21:18:38 +0700

    tao route va function cho Post Method

--- app/Http/Controllers/backend/CauHinhController.php
+++ app/Http/Controllers/backend/CauHinhController.php
@@ -39,4 +39,37 @@ class CauHinhController extends Controller
     {
         return view('backend.pages.cau_hinh.tieu_de');
     }
+
+    public function saveConfig_bannerTop()
+    {
+        return view('backend.pages.cau_hinh.banner_top');
+    }
+    public function saveConfig_apiVnPay()
+    {
+        return view('backend.pages.cau_hinh.api_vn_pay');
+    }
+    public function saveConfig_apiZalo()
+    {
+        return view('backend.pages.cau_hinh.api_zalo');
+    }
+    public function saveConfig_footer()
+    {
+        return view('backend.pages.cau_hinh.footer');
+    }
+    public function saveConfig_logo()
+    {
+        return view('backend.pages.cau_hinh.logo');
+    }
+    public function saveConfig_menu()
+    {
+        return view('backend.pages.cau_hinh.menu');
+    }
+    public function saveConfig_smtpEmail()
+    {
+        return view('backend.pages.cau_hinh.smtp_email');
+    }
+    public function saveConfig_tieuDe()
+    {
+        return view('backend.pages.cau_hinh.tieu_de');
+    }
 }
