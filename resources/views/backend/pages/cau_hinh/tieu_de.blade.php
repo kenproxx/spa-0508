860b5d196c862ddc09a6a6b8600a911c97dc6a14
kienh <kienhue98@gmail.com>
2023-08-18 20:41:17 +0700

    ckeditor cau hinh tieu de

--- resources/views/backend/pages/cau_hinh/tieu_de.blade.php
+++ resources/views/backend/pages/cau_hinh/tieu_de.blade.php
@@ -9,19 +9,19 @@
             <form action="{{ route('backend.cau-hinh.store') }}" method="post">
                 @csrf
                 <div class="row">
-                    <div class="col-md-6 mb-3">
+                    <div class="col-md-12 mb-3">
                         <label for="tieu_de">Tiêu đề</label>
                         <input type="text" class="form-control" id="tieu_de" name="tieu_de" placeholder="Nhập tiêu đề" value="{{ $arrConfig['tieu_de'] ?? '' }}"/>
                     </div>
-                    <div class="col-md-6 mb-3">
+                    <div class="col-md-12 mb-3">
                         <label for="mo_ta_web">Mô tả web</label>
-                        <input type="text" class="form-control" id="mo_ta_web" name="mo_ta_web" placeholder="Nhập mô tả web" value="{{ $arrConfig['mo_ta_web'] ?? '' }}"/>
+                        <textarea cols="80" id="mo_ta_web" name="mo_ta_web" rows="10" data-sample="1" data-sample-short>{{ $arrConfig['mo_ta_web'] ?? '' }}</textarea>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-12 mb-3">
                         <label for="script_code">Mã script</label>
-                        <textarea type="text" class="form-control" id="script_code" name="script_code" placeholder="Nhập mã script" >{{ $arrConfig['script_code'] ?? '' }}</textarea>
+                        <textarea cols="80" id="script_code" name="script_code" rows="10" data-sample="1" data-sample-short>{{ $arrConfig['script_code'] ?? '' }}</textarea>
                     </div>
                 </div>
                  <input type="hidden" name="type" value="{{ \App\Enum\ConfigType::TIEU_DE }}">
@@ -31,6 +31,15 @@
             </form>
         </div>
     </div>
-
+    <script src="../../dist/libs/ckeditor/ckeditor.js"></script>
+    <script src="../../dist/libs/ckeditor/samples/js/sample.js"></script>
+    <script data-sample="1">
+        CKEDITOR.replace("mo_ta_web", {
+            height: 150,
+        });
+        CKEDITOR.replace("script_code", {
+            height: 150,
+        });
+    </script>
 @endsection
 
