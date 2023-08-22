34f6621c4b515e323c7cdda126fd2b4502cbfd37
kienh <kienhue98@gmail.com>
2023-08-22 13:54:10 +0700

    sua route

---
+++ routes/backend.php
@@ -0,0 +1,90 @@
+<?php
+
+use App\Http\Controllers\backend\AgencyController;
+use App\Http\Controllers\backend\CauHinhController;
+use App\Http\Controllers\backend\DonHangController;
+use App\Http\Controllers\backend\MoreServiceController;
+use App\Http\Controllers\backend\ProductController;
+use App\Http\Controllers\backend\ProfileController;
+use App\Http\Controllers\backend\QL_UserController;
+use App\Http\Controllers\backend\QL_VoucherController;
+use App\Http\Controllers\backend\QLEmailController;
+use App\Http\Controllers\backend\ThongKeController;
+use App\Http\Controllers\backend\TinNhanController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::prefix('/nguoi-dung')->group(function () {
+    Route::get('', [QL_UserController::class, 'index'])->name('backend.user.show');
+    Route::post('/store', [QL_UserController::class, 'store'])->name('backend.nguoi-dung.store');
+    Route::get('/show/{id}', [QL_UserController::class, 'show'])->name('backend.nguoi-dung.show');
+    Route::post('/update/{id}', [QL_UserController::class, 'update'])->name('backend.nguoi-dung.update');
+    Route::get('/destroy/{id}', [QL_UserController::class, 'destroy'])->name('backend.nguoi-dung.destroy');
+});
+
+Route::prefix('/dai-ly')->group(function () {
+    Route::get('', [AgencyController::class, 'index'])->name('backend.dai-ly.show');
+});
+
+Route::prefix('/thong-ke')->group(function () {
+    Route::get('', [ThongKeController::class, 'index'])->name('backend.thong-ke.show');
+});
+
+Route::prefix('/tin-nhan')->group(function () {
+    Route::get('', [TinNhanController::class, 'index'])->name('backend.tin-nhan.show');
+});
+
+Route::prefix('/email')->group(function () {
+    Route::get('', [QLEmailController::class, 'index'])->name('backend.email.show');
+});
+
+Route::prefix('/ma-giam-gia')->group(function () {
+    Route::get('', [QL_VoucherController::class, 'index'])->name('backend.voucher.show');
+    Route::post('/store', [QL_VoucherController::class, 'store'])->name('backend.voucher.store');
+    Route::delete('/destroy/{id}', [QL_VoucherController::class, 'destroy'])->name('backend.voucher.destroy');
+});
+
+Route::prefix('/san-pham')->group(function () {
+    Route::get('', [ProductController::class, 'index'])->name('backend.san-pham.show');
+    Route::get('/create', [ProductController::class, 'create'])->name('backend.san-pham.create');
+    Route::post('/store', [ProductController::class, 'store'])->name('backend.san-pham.store');
+});
+
+Route::prefix('/dich-vu-them')->group(function () {
+    Route::get('', [MoreServiceController::class, 'index'])->name('backend.dich-vu-them.show');
+    Route::post('/store', [MoreServiceController::class, 'store'])->name('backend.dich-vu-them.store');
+    Route::get('/get-by-agency/{id}', [MoreServiceController::class, 'getAllByAgency'])->name('backend.dich-vu-them.get-by-agency');
+    Route::get('/{id}', [MoreServiceController::class, 'show'])->name('backend.dich-vu-them.show.id');
+    Route::get('/destroy/{id}', [MoreServiceController::class, 'destroy'])->name('backend.dich-vu-them.destroy');
+});
+
+Route::prefix('/trang-ca-nhan')->group(function () {
+    Route::get('', [ProfileController::class, 'index'])->name('backend.trang-ca-nhan.show');
+});
+
+Route::prefix('/don-hang')->group(function () {
+    Route::get('', [DonHangController::class, 'index'])->name('backend.don-hang.show');
+});
+
+Route::prefix('/cau-hinh')->group(function () {
+    Route::get('/{view}', [CauHinhController::class, 'index'])->name('backend.cau-hinh.show');
+    Route::post('', [CauHinhController::class, 'store'])->name('backend.cau-hinh.store');
+    Route::post('/logo', [CauHinhController::class, 'saveLogoConfig'])->name('backend.cau-hinh.logo.store');
+});
+
+
+
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -30,72 +30,16 @@ Route::post('/login', [AuthController::class, 'login'])->name('sign-in');
 Route::get('/logout', [AuthController::class, 'logout'])->name('log-out');
 
 
-Route::middleware(['role.admin.super_admin'])->group(function () {
-
-    Route::prefix('/backend')->group(function () {
-
-        Route::prefix('/nguoi-dung')->group(function () {
-            Route::get('', [QL_UserController::class, 'index'])->name('backend.user.show');
-            Route::post('/store', [QL_UserController::class, 'store'])->name('backend.nguoi-dung.store');
-            Route::get('/show/{id}', [QL_UserController::class, 'show'])->name('backend.nguoi-dung.show');
-            Route::post('/update/{id}', [QL_UserController::class, 'update'])->name('backend.nguoi-dung.update');
-            Route::get('/destroy/{id}', [QL_UserController::class, 'destroy'])->name('backend.nguoi-dung.destroy');
-        });
-
-        Route::prefix('/dai-ly')->group(function () {
-            Route::get('', [AgencyController::class, 'index'])->name('backend.dai-ly.show');
-        });
-
-        Route::prefix('/thong-ke')->group(function () {
-            Route::get('', [ThongKeController::class, 'index'])->name('backend.thong-ke.show');
-        });
-
-        Route::prefix('/tin-nhan')->group(function () {
-            Route::get('', [TinNhanController::class, 'index'])->name('backend.tin-nhan.show');
-        });
-
-        Route::prefix('/email')->group(function () {
-            Route::get('', [QLEmailController::class, 'index'])->name('backend.email.show');
-        });
-
-        Route::prefix('/ma-giam-gia')->group(function () {
-            Route::get('', [QL_VoucherController::class, 'index'])->name('backend.voucher.show');
-            Route::post('/store', [QL_VoucherController::class, 'store'])->name('backend.voucher.store');
-            Route::delete('/destroy/{id}', [QL_VoucherController::class, 'destroy'])->name('backend.voucher.destroy');
-        });
-
-        Route::prefix('/san-pham')->group(function () {
-            Route::get('', [ProductController::class, 'index'])->name('backend.san-pham.show');
-            Route::get('/create', [ProductController::class, 'create'])->name('backend.san-pham.create');
-            Route::post('/store', [ProductController::class, 'store'])->name('backend.san-pham.store');
-        });
-
-        Route::prefix('/dich-vu-them')->group(function () {
-            Route::get('', [MoreServiceController::class, 'index'])->name('backend.dich-vu-them.show');
-            Route::post('/store', [MoreServiceController::class, 'store'])->name('backend.dich-vu-them.store');
-            Route::get('/get-by-agency/{id}', [MoreServiceController::class, 'getAllByAgency'])->name('backend.dich-vu-them.get-by-agency');
-            Route::get('/{id}', [MoreServiceController::class, 'show'])->name('backend.dich-vu-them.show.id');
-            Route::get('/destroy/{id}', [MoreServiceController::class, 'destroy'])->name('backend.dich-vu-them.destroy');
-        });
-
-        Route::prefix('/trang-ca-nhan')->group(function () {
-            Route::get('', [ProfileController::class, 'index'])->name('backend.trang-ca-nhan.show');
-        });
-
-        Route::prefix('/don-hang')->group(function () {
-            Route::get('', [DonHangController::class, 'index'])->name('backend.don-hang.show');
-        });
-
-        Route::prefix('/cau-hinh')->group(function () {
-            Route::get('/{view}', [CauHinhController::class, 'index'])->name('backend.cau-hinh.show');
-            Route::post('', [CauHinhController::class, 'store'])->name('backend.cau-hinh.store');
-            Route::post('/logo', [CauHinhController::class, 'saveLogoConfig'])->name('backend.cau-hinh.logo.store');
-        });
-    });
+Route::group(['prefix' => 'backend', 'middleware' => 'role.admin.super_admin'], function () {
+    require_once __DIR__ . '/backend.php';
 });
 
-Route::prefix('/frontend')->group(function () {
-    Route::get('', [HomePageController::class, 'index'])->name('frontend.index');
+Route::group(['prefix' => 'frontend'], function () {
+    require_once __DIR__ . '/frontend.php';
 });
+
+
+
+
 
 
