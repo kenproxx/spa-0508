3ef66d6c29b1af2b1a7b215910202fab61127fb0
kienh <kienhue98@gmail.com>
2023-08-22 17:34:26 +0700

    get list config

--- app/Http/Controllers/frontend/HomePageController.php
+++ app/Http/Controllers/frontend/HomePageController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers\frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Config;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Storage;
 
 class HomePageController extends Controller
 {
     public function index()
     {
-        return response()->json(123);
+        $listConfig = Config::all();
+        Session::put('config', $listConfig);
+        return view('backend/pages/thong-ke/index');
     }
 }
