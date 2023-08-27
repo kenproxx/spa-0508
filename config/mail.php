14765cd7d58ff8409498957c2a998654b787ef19
dainq <97323194+dainqd@users.noreply.github.com>
2023-08-28 02:12:33 +0700

    Call mail from db to send mail.

--- config/mail.php
+++ config/mail.php
@@ -1,49 +1,30 @@
 <?php
 
+use App\Models\Config;
+
+$mailConfigs = Config::where('type', 'smtp_email')->whereIn('name', [
+    'MAIL_USERNAME', 'MAIL_DRIVER', 'MAIL_PORT', 'MAIL_PASSWORD', 'MAIL_HOST', 'MAIL_ENCRYPTION'
+])->get();
+
+$mailerSettings = [];
+
+foreach ($mailConfigs as $config) {
+    $mailerSettings[$config->name] = $config->value;
+}
+
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Default Mailer
-    |--------------------------------------------------------------------------
-    |
-    | This option controls the default mailer that is used to send any email
-    | messages sent by your application. Alternative mailers may be setup
-    | and used as needed; however, this mailer will be used by default.
-    |
-    */
-
     'default' => env('MAIL_MAILER', 'smtp'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Mailer Configurations
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure all of the mailers used by your application plus
-    | their respective settings. Several examples have been configured for
-    | you and you are free to add your own as your application requires.
-    |
-    | Laravel supports a variety of mail "transport" drivers to be used while
-    | sending an e-mail. You will specify which one you are using for your
-    | mailers below. You are free to add additional mailers as required.
-    |
-    | Supported: "smtp", "sendmail", "mailgun", "ses", "ses-v2",
-    |            "postmark", "log", "array", "failover"
-    |
-    */
-
     'mailers' => [
-        'smtp' => [
-            'transport' => 'smtp',
+        $mailerSettings['MAIL_DRIVER'] => [
+            'transport' => $mailerSettings['MAIL_DRIVER'],
             'url' => env('MAIL_URL'),
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'host' => $mailerSettings['MAIL_HOST'],
+            'port' => $mailerSettings['MAIL_PORT'],
+            'encryption' => $mailerSettings['MAIL_ENCRYPTION'],
+            'username' => $mailerSettings['MAIL_USERNAME'],
+            'password' => $mailerSettings['MAIL_PASSWORD'],
             'timeout' => null,
-            'local_domain' => env('MAIL_EHLO_DOMAIN'),
         ],
 
         'ses' => [
@@ -87,36 +68,13 @@ return [
         ],
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Global "From" Address
-    |--------------------------------------------------------------------------
-    |
-    | You may wish for all e-mails sent by your application to be sent from
-    | the same address. Here, you may specify a name and address that is
-    | used globally for all e-mails that are sent by your application.
-    |
-    */
-
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Markdown Mail Settings
-    |--------------------------------------------------------------------------
-    |
-    | If you are using Markdown based email rendering, you may configure your
-    | theme and component paths here, allowing you to customize the design
-    | of the emails. Or, you may simply stick with the Laravel defaults!
-    |
-    */
-
     'markdown' => [
         'theme' => 'default',
-
         'paths' => [
             resource_path('views/vendor/mail'),
         ],
